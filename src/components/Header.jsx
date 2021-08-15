68f89c7cfa84dd6e35fe6631a1d8d107f0cc5f91
SagingaliMamay <sagingali.mamayev@gmail.com>
2021-08-15 15:42:20 +0200

    commit by web storm

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+//component is the function who return the jsx
+const Header = () => {
+    return (
+        <header className={'head'}>
+            <div className={'logo'}>
+                <img
+                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRA5g0EupBwk1DdPljTmVBofqTfrecmMvEX7Q&usqp=CAU"
+                    alt=""/>
+
+            <div>This is header</div>
+            </div>
+        </header>
+)
+};
+export default Header;
