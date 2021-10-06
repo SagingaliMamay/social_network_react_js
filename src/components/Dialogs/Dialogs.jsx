031706e29efcdaf9939be655b5ea788a70eb3843
SagingaliMamay <sagingali.mamayev@gmail.com>
2021-10-06 13:46:30 +0200

    route

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,27 +1,38 @@
 import React from 'react';
-import style from './Dialogs.module.css'
+import style from './Dialogs.module.css';
+import {NavLink} from 'react-router-dom';
 
+const DialogItem = (props) =>{
+   // let path = "/dialogs/" + props.id;
+    return(
+        <div className={style.dialog + '' + style.active}>
+                    
+        <NavLink to ={"/dialogs/" + props.id}>{props.name}</NavLink>
+     </div>
+    )
+}
 
 const Dialogs = (props) => {
     return (
         <div className={style.dialogs}>
             <div className={style.dialogsItems}>
+                <DialogItem name="Dimasik" id ="1"/>
+                
                 <div className={style.dialog}>
-                    Dymich
+                  <NavLink to ="/dialogs/2" >Lena</NavLink>  
                 </div>
                 <div className={style.dialog}>
-                    Lena
+                   <NavLink to ="/dialogs/3" >Igor</NavLink>
                 </div>
                 <div className={style.dialog}>
-                    Igor
+                  <NavLink to ="/dialogs/4" >Sasha</NavLink>
                 </div>
                 <div className={style.dialog}>
-                    Sasha
+                  <NavLink to ="/dialogs/5" >Ivan</NavLink>
                 </div>
-                <div className={style.dialog}>
-                    Ivan
-                </div>
-                <div className={style.messages}>
+           
+            </div>
+            <div className={style.messages}>
                     <div className={style.dialog}>Hi</div>
 
 
@@ -30,8 +41,7 @@ const Dialogs = (props) => {
 
                     <div className={style.dialog}>Super coder</div>
 
-                </div>
-            </div>
+                </div>    
         </div>
     )
 }
