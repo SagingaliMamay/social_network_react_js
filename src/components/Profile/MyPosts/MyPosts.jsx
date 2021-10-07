3e6822a7ccb7ff54ed89c9a37f404cca3a3f699d
SagingaliMamay <sagingali.mamayev@gmail.com>
2021-10-07 14:32:59 +0200

    code refactoring with posts data

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,6 +3,11 @@ import style from './MyPosts.module.css';
 import Post from './Post/Post';
 
 const MyPosts = (props) => {
+    let postsData = [
+        {id:1,post:'You are super React Developer',likes:150},
+        {id:2,post:"You are a really good learner",likes:15},
+        {id:3,post:'I am quick learner',likes:180}
+    ];
     return (
 
         <div className={style.postBlock}>
@@ -17,9 +22,9 @@ const MyPosts = (props) => {
                 </div>
             </div>
             <div className={style.posts}>
-                <Post message="You are super React Developer" likeCounter="20" />
-                <Post message="You are a really good learner" likeCounter="150" />
-                <Post motivation="You are the most disciplened man" likeCounter="15" />
+                <Post message={postsData[0].post} id={postsData[0].id} likeCounter = {postsData[0].likes} />
+                <Post message={postsData[1].post} id={postsData[1].id} likeCounter = {postsData[1].likes} />
+                <Post message={postsData[2].post} id={postsData[2].id} likeCounter = {postsData[2].likes} />
             </div>
         </div>
     )
